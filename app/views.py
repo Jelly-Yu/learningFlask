5ccb357e9735b46ebd0eafd83c5c4b55f284f34e
Jelly-Yu <jelly.yu1992@gmail.com>
2016-02-09 16:02:24 -0500

    part2 templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "hello, world!"
+	user = {'nickname':'Iris'}
+	posts = [
+		{
+			'author': {'nickname': 'Jelly'},
+			'body': 'It is cold outside'
+
+		},
+		{
+			'author':{'nickname':'Jane'},
+			'body': 'The snow was so heavy!'
+		}
+	]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
